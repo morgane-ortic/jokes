80c6d403571269aa33bb932a44bcff70b810a7a4
whatmeansgithub <drinkyoghurt@gmail.com>
2024-03-15 15:02:18 +0100

    random joke generator

---
+++ joke_generator.py
@@ -0,0 +1,17 @@
+import random
+
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them!",
+    "Why don't skeletons fight each other? They don't have the guts!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "What do you call a fish wearing a crown? King Neptune!",
+    "Why don't eggs tell jokes? Because they might crack up!",
+    "How do you organize a space party? You planet!",
+    "Why did the bicycle fall over? Because it was two-tired!",
+    "What do you call a bear with no teeth? A gummy bear!",
+    "Why did the tomato turn red? Because it saw the salad dressing!"
+]
+
+random_joke = random.choice(jokes)
+print(random_joke)
