9dbaf93d439a93fd6f3ddf6b83832e689f637f11
Erick Meyer <eagalvez129@gmail.com>
2024-03-15 15:34:20 +0100

    new changes

--- Erick_joke.py
+++ Erick_joke.py
@@ -1,2 +1,5 @@
-print("In Denver, the members of a Sunday­-school class were asked to set down their favorite biblical truths.") 
-print("One youngster laboriously printed: “Do one to others as others do one to you.” —Lee Olson, The Denver Post")
+msg1 = "In Denver, the members of a Sunday­-school class were asked to set down their favorite biblical truths."
+msg2 = "One youngster laboriously printed: “Do one to others as others do one to you.” —Lee Olson, The Denver Post"
+
+msg = msg1 + msg2
+print(msg)
