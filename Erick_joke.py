3526d397e71d62599b131f7eca7665e84d0b01d5
Erick Meyer <eagalvez129@gmail.com>
2024-03-15 15:19:45 +0100

    New Joke

--- Erick_joke.py
+++ Erick_joke.py
@@ -1,2 +1,2 @@
-print("In Denver, the members of a Sunday­-school class were asked to set down their favorite biblical truths.)" 
+print("In Denver, the members of a Sunday­-school class were asked to set down their favorite biblical truths.") 
 print("One youngster laboriously printed: “Do one to others as others do one to you.” —Lee Olson, The Denver Post")
