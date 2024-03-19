0ec80ddea488a21f8eb040fe3187055716cdd4e3
Erick Meyer <eagalvez129@gmail.com>
2024-03-19 09:26:24 +0100

    New Joke

--- Erick_joke.py
+++ Erick_joke.py
@@ -1,5 +1,7 @@
-msg1 = "In Denver, the members of a Sunday­-school class were asked to set down their favorite biblical truths."
-msg2 = "One youngster laboriously printed: “Do one to others as others do one to you.” —Lee Olson, The Denver Post"
+msg1 = " Did you hear about the first restaurant to open on the moon?"
+msg2 = "It had great food, but no atmosphere."
 
 msg = msg1 + msg2
 print(msg)
+
+
