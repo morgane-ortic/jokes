a01b5c629750264d6270aeb78e76a7ffe49daedf
Erick Meyer <eagalvez129@gmail.com>
2024-03-19 09:59:08 +0100

    Cowsay joke

--- Erick_joke.py
+++ Erick_joke.py
@@ -1,8 +1,6 @@
 import subprocess
 
-msg1 = " Did you hear about the first restaurant to open on the moon?"
-msg2 = "It had great food, but no atmosphere."
+msg = "Did you hear about the first restaurant to open on the moon? It had great food, but no atmosphere."
 
-msg = msg1 + msg2
 subprocess.call(["cowsay", msg])
 
